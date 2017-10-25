efdad9ac8f29e6850086e316bcd3ca0fcee598c0
Tushar S <tushars8384@gmail.com>
2017-10-25 11:43:02 +0530

    Add files via upload

---
+++ successmessage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Insert Data</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+  <div class="container">
+  	<div class="row">
+  	<div class="col-sm-4">
+  		<h3><?php echo $message  ?></h3>
+    
+  	<a href="http://localhost/start/index.php/Home_insert/index">Add Data</a><br>
+    <a href="http://localhost/start/index.php/Update_control/index">Update Data</a><br>
+    <a href="http://localhost/start/index.php/Delete_control/index">Delete Data</a><br>
+
+
+
+  	</div>
+  	</div>
+  </div>
+</body>
+</html>
